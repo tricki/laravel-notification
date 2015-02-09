937ff9c2042981448ee930cd8c74c60d532d53c1
Thomas Rickenbach <thomasrickenbach@gmail.com>
2015-02-09 07:49:07 +0100

    make column read_at nullable

---
+++ src/migrations/2015_02_08_132023_make_read_at_nullable.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeReadAtNullable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::statement('ALTER TABLE `notification_user` MODIFY `read_at` TIMESTAMP NULL DEFAULT NULL;');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement('ALTER TABLE `notification_user` MODIFY `read_at` TIMESTAMP NOT NULL DEFAULT  \'0000-00-00 00:00:00\';');
+	}
+
+}
